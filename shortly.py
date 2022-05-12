e7fe7d5eb0dba4a1ae217db15dd2990b8db18c4d
shant <prashantghimire79@gmail.com>
2022-05-12 06:36:08 +0545

    fix some issues

--- shortly.py
+++ shortly.py
@@ -3,6 +3,7 @@ from urllib import response
 import redis
 from werkzeug.urls import url_parse
 from werkzeug.wrappers import Request, Response
+from werkzeug.routing import Map, Rule
 from werkzeug.exceptions import HTTPException, NotFound
 from werkzeug.middleware.shared_data import SharedDataMiddleware
 from werkzeug.utils import redirect
@@ -15,7 +16,7 @@ class Shortly(object):
         self.redis = redis.Redis(
             config['redis_host'], config['redis_port'])
         template_path = os.path.join(os.path.dirname(__file__), 'templates')
-        self.jinja_env = Environment(Loader=FileSystemLoader(template_path),
+        self.jinja_env = Environment(loader=FileSystemLoader(template_path),
         autoescape=True)
 
         self.url_map = Map([
@@ -43,6 +44,64 @@ class Shortly(object):
 
     def __call__(self, environ, start_response):
         return self.wsgi_app(environ, start_response)
+    
+    def is_valid_url(self, url):
+        parts = url_parse(url)
+        return parts.scheme in ('http', 'https')
+    
+    def on_new_url(self, request):
+        error = None
+        url = ''
+        if request.method == 'POST':
+            url = request.form['url']
+            if not self.is_valid_url(url):
+                error = 'Please enter a valid URL'
+            else:
+                short_id = self.insert_url(url)
+                return redirect(f"/{short_id}+")
+        return self.render_template('new_url.html', error=error, url=url)
+
+    
+
+    def insert_url(self, url):
+        short_id = self.redis.get(f'reverse-url:{url}')
+        if short_id is not None:
+            return short_id
+        url_num = self.redis.incr('last-url-id')
+        short_id = base36_encode(url_num)
+        self.redis.set(f'url-target:{short_id}', url)
+        self.redis.set(f'reverse-url:{url}', short_id)
+        return short_id
+
+    def on_follow_short_link(self, request, short_id):
+        link_target = self.redis.get(f'url-target:{short_id}')
+        if link_target is None:
+            raise NotFound()
+        self.redis.incr(f'click-count{short_id}')
+        print('link_target===========', link_target)
+        print('link_target===========', type(link_target))
+        return redirect(link_target)
+
+    def on_short_link_details(self, request, short_id):
+        link_target = self.redis.get(f'url-target:{short_id}')
+        if link_target is None:
+            raise NotFound()
+        click_count = int(self.redis.get(f'click-count:{short_id}') or 0)
+        return self.render_template('short_link_details.html',
+            link_target=link_target,
+            short_id=short_id,
+            click_count=click_count
+        )
+
+def base36_encode(number):
+    assert number >=0, 'positive integer required'
+    if number == 0:
+        return '0'
+    base36 = []
+    while number != 0:
+        number, i = divmod(number, 36)
+        base36.append('0123456789abcdefghijklmnopqrstuvwxyz'[i])
+    return ''.join(reversed(base36))
 
 
 def create_app(redis_host="localhost", redis_port=6379, with_static=True):
@@ -56,59 +115,7 @@ def create_app(redis_host="localhost", redis_port=6379, with_static=True):
         })
     return app
 
-def on_new_url(self, request):
-    error = None
-    url = ''
-    if request.method == 'POST':
-        url = request.form['url']
-        if not is_valid_url(url):
-            error = 'Please enter a valid URL'
-        else:
-            short_id = self.insert_url(url)
-            return redirect(f"/{short_id}+")
-    return self.render_template('new_url.html', error=error, url=url)
 
-def is_valid_url(self, url):
-    parts = url_parse(url)
-    return parts.scheme in ('http', 'https')
-
-def insert_url(self, url):
-    short_id = self.redis.get(f'reverse-url:{url}')
-    if short_id is not None:
-        return short_id
-    url_num = self.redis.incr('last-url-id')
-    short_id = base36_encode(url_num)
-    self.redis.set(f'url-target:{short_id}', url)
-    self.redis.set(f'reverse-url:{url}', short_id)
-    return short_id
-
-def base36_encode(number):
-    assert number >=0, 'positive integer required'
-    if number == 0:
-        return '0'
-    base36 = []
-    while number != 0:
-        number, i = divmod(number, 36)
-        base36.append('0123456789abcdefghijklmnopqrstuvwxyz'[i])
-    return ''.join(reversed(base36))
-
-def on_follow_short_link(self, request, short_id):
-    link_target = self.redis.get(f'url-target:{short_id}')
-    if link_target is None:
-        raise NotFound()
-    self.redis.incr(f'click-count{short_id}')
-    return redirect(link_target)
-
-def on_short_link_details(self, request, short_id):
-    link_target = self.redis.get(f'url-target:{short_id}')
-    if link_target is None:
-        raise NotFound()
-    click_count = int(self.redis.get(f'click-count:{short_id}') or 0)
-    return self.render_template('short_link_details.html',
-        link_target=link_target,
-        short_id=short_id,
-        click_count=click_count
-    )
 
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
